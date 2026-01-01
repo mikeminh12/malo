ff4119b8e242cea370966bd17532f4cab4704a9f
mikeminh12 <hoquocminh6a1@gmail.com>
2026-01-01 16:46:42 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyC9LeRT5K43R519zxfFknu1QAr5_-3jQ",
+  authDomain: "malo-c5b59.firebaseapp.com",
+  projectId: "malo-c5b59",
+  storageBucket: "malo-c5b59.firebasestorage.app",
+  messagingSenderId: "785455852092",
+  appId: "1:785455852092:web:0bd514152a4f4fcb4ff139"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
